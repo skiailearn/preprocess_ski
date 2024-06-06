ec6d52807ace133df68a951035779f42ae704015
skiailearn <skiailearn@gmail.com>
2024-06-05 17:19:43 -0700

    version increased

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
 	name = 'preprocess_ski', #this should be unique
-	version = '0.0.1',
+	version = '0.0.2',
 	author = 'Santhosh I',
 	author_meail = 'skiailearn@gmail.com',
 	description = 'This is preprocessing package',
